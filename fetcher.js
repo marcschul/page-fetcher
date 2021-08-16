d9b6ae1ab93efd3e471caaac1630390b85d81204
marcschul <marcschulm@gmail.com>
2021-08-16 18:59:27 +0000

    added logic to request, overwrite cmd

--- fetcher.js
+++ fetcher.js
@@ -1,15 +1,38 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
 const input = process.argv.slice(2); // index2++
 const url = input[0];
 const filePath = input[1];
 
 request(url, (error, response, body) => {
-  fs.writeFileSync(filePath, body, function (err) {
-    if (err) return console.log(err);
+  const fileExists = fs.existsSync(filePath);
+  if (fileExists === false) {
+    fs.writeFileSync(filePath, body, function (err) {
+      if (err) return console.log(err);
+      });
+    const stats = fs.statSync(filePath);
+    const bytes = stats.size;
+    console.log(`Downloaded and saved ${bytes} bytes to ${filePath}`);
+  } else {
+    let answer = 'y';
+    let answer1 = 'Y';
+    const rl = readline.createInterface({input: process.stdin,
+                                         output: process.stdout});
+    rl.question(`Do you want to overwrite ${filePath}?`, (userInput) => {
+      if (userInput.trim() === answer || userInput.trim() === answer1) {
+        fs.writeFileSync(filePath, body, function (err) {
+          if (err) return console.log(err);
+          });
+        const stats = fs.statSync(filePath);
+        const bytes = stats.size;
+        console.log(`Overwritten and saved ${bytes} to ${filePath}`);
+        rl.close();
+      } else {
+        console.log(`Did not overwrite ${filePath}`)
+        rl.close();
+      }
     });
-  const stats = fs.statSync(filePath);
-  const bytes = stats.size;
-  console.log(`Downloaded and saved ${bytes} bytes to ${filePath}`);
+  }
 });
